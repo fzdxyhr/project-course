8211e59da426fb963c7607721acccb5c1acdc88c
fzdxyhr <1361579931@qq.com>
2020-05-26 10:42:31 +0800

    账号密码修复

--- course/src/main/java/com/yhr/course/course/config/AliyunFileHandle.java
+++ course/src/main/java/com/yhr/course/course/config/AliyunFileHandle.java
@@ -20,8 +20,8 @@ public class AliyunFileHandle {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAIe9UrTlT4zGIF";
-        String accessKeySecret = "Yd4CzgTW4iPOiuxFtiZzhDFp0SVdQ0";
+        String accessKeyId = "#################";
+        String accessKeySecret = "***************";
 
         // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
